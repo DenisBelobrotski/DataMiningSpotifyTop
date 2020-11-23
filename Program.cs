e5e532b64bb154c9c264a8a1523d2bc6f3ff829a
Denis_Belobrotski <denis.b@playgendary.com>
2020-11-23 21:53:09 +0300

    Hello world added

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace DataMiningSpotifyTop
+﻿using System;
+
+namespace DataMiningSpotifyTop
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello, world!");
         }
     }
 }
